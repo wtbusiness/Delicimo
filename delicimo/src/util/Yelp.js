781e4a873a01476ea9110486aeed1f0b0ddaa376
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-02-08 15:17:32 -0500

    debugging

---
+++ delicimo/src/util/Yelp.js
@@ -0,0 +1,35 @@
+const apiKey = '4LLDFa7Tand8dbpoDjoRBepI_mI1EmgKAN9xhm3aHmnjxqXmlorY19ZCf3_hsG3znfYgCrz_nBKuBShxP9LXVile_PUWXi3yfxLejTzEI9zFb4686va_HAmAYzhYY3Yx'
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
