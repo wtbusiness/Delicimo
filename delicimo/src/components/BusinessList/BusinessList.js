21a004cdd5805160748366f9469c1eb4eb38b81e
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-02-08 10:10:56 -0500

    framework of app

--- delicimo/src/components/BusinessList/BusinessList.js
+++ delicimo/src/components/BusinessList/BusinessList.js
@@ -5,15 +5,17 @@ import Business from "../Business/Business";
 class BusinessList extends React.Component {
     render() {
         return (
-        <div className="BusinessList">
-            <Business  /> 
-            <Business / >
-            <Business / >
-            <Business / >
-            <Business / >
-            <Business / >
-        </div>
-            )
+          <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+          </div>
+        );
     }
 }
 
