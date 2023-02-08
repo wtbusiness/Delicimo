b66403e260c52243f3419e0736e47c2935485a99
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-02-08 13:25:50 -0500

    search working

--- delicimo/src/components/SearchBar/SearchBar.js
+++ delicimo/src/components/SearchBar/SearchBar.js
@@ -17,6 +17,7 @@ class SearchBar extends React.Component {
     };
     this.handleTermChange = this.handleTermChange.bind(this);
      this.handleLocationChange = this.handleLocationChange.bind(this);
+     this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSearch(event) {
@@ -69,7 +70,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a href="www.#.com">Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
