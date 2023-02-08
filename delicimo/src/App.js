b6303a29a1355459d430fb39ef33f6c485ee7924
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-02-08 16:14:28 -0500

    Delicimo App

--- delicimo/src/App.js
+++ delicimo/src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Delicimo</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
--- delicimo/src/util/Yelp.js
+++ delicimo/src/util/Yelp.js
@@ -4,7 +4,8 @@ const apiKey =
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}
+`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`,
