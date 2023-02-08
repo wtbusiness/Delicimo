3ea3eabff29044294db30a416d463720d8f79393
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-02-08 09:38:27 -0500

    finishing up SearchBar

--- delicimo/src/components/SearchBar/SearchBar.js
+++ delicimo/src/components/SearchBar/SearchBar.js
@@ -6,3 +6,31 @@ const sortByOptions = {
     'Highest Rated' : 'rating',
     'Most Reviewed' : 'review_count'
 };
+
+class searchBar extends React.Component {
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
+
+    render() {
+        return (
+            <div class="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+     {this.renderSortByOptions()}
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Let's Go</a>
+  </div>
+</div>
+        )
+    }
+}
